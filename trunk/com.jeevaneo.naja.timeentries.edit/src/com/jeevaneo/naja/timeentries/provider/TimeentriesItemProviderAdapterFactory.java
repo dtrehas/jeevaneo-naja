b15a39fd5d382a7aabe418fc63752cacf7bc564e
alexandre.borgoltz <alexandre.borgoltz@9d19b4e0-ff7f-11dd-984d-ab748d24aa7e>
2009-03-12 02:36:46 +0000

    aborgoltz: now the generated Imputation for a TimeEntry is automatically linked to a planif, if found

--- trunk/com.jeevaneo.naja.timeentries.edit/src/com/jeevaneo/naja/timeentries/provider/TimeentriesItemProviderAdapterFactory.java
+++ trunk/com.jeevaneo.naja.timeentries.edit/src/com/jeevaneo/naja/timeentries/provider/TimeentriesItemProviderAdapterFactory.java
@@ -93,14 +93,6 @@ public class TimeentriesItemProviderAdapterFactory extends TimeentriesAdapterFac
 	public Adapter createTimeEntriesAdapter() {
 		if (timeEntriesItemProvider == null) {
 			timeEntriesItemProvider = new TimeEntriesItemProvider(this);
-			timeEntriesItemProvider.addListener(new INotifyChangedListener() {
-				
-				@Override
-				public void notifyChanged(Notification notification) {
-					System.out.println("yeah " + notification);
-			
-				}
-			});
 		}
 
 		return timeEntriesItemProvider;
@@ -124,14 +116,6 @@ public class TimeentriesItemProviderAdapterFactory extends TimeentriesAdapterFac
 	public Adapter createTimeEntryAdapter() {
 		if (timeEntryItemProvider == null) {
 			timeEntryItemProvider = new TimeEntryItemProvider(this);
-			timeEntryItemProvider.addListener(new INotifyChangedListener() {
-			
-				@Override
-				public void notifyChanged(Notification notification) {
-					System.out.println("yo " + notification);
-			
-				}
-			});
 		}
 
 		return timeEntryItemProvider;
