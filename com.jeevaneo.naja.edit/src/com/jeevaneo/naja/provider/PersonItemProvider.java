533a59062ad66b5712b819fa4c8d479bc5f100fa
alexandre.borgoltz <alexandre.borgoltz@9d19b4e0-ff7f-11dd-984d-ab748d24aa7e>
2009-03-12 15:14:45 +0000

    aborgoltz: added a max load per day at the Person level

--- com.jeevaneo.naja.edit/src/com/jeevaneo/naja/provider/PersonItemProvider.java
+++ com.jeevaneo.naja.edit/src/com/jeevaneo/naja/provider/PersonItemProvider.java
@@ -73,6 +73,7 @@ public class PersonItemProvider
 			addPlanificationsPropertyDescriptor(object);
 			addAvailabilityStartDatePropertyDescriptor(object);
 			addLastDatePropertyDescriptor(object);
+			addMaxLoadPerDayPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
@@ -254,6 +255,28 @@ public class PersonItemProvider
 	}
 
 	/**
+	 * This adds a property descriptor for the Max Load Per Day feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addMaxLoadPerDayPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_Person_maxLoadPerDay_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_Person_maxLoadPerDay_feature", "_UI_Person_type"),
+				 NajaPackage.Literals.PERSON__MAX_LOAD_PER_DAY,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
 	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
 	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
 	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
@@ -326,6 +349,7 @@ public class PersonItemProvider
 			case NajaPackage.PERSON__LEFT_AVAILABILITY:
 			case NajaPackage.PERSON__AVAILABILITY_START_DATE:
 			case NajaPackage.PERSON__LAST_DATE:
+			case NajaPackage.PERSON__MAX_LOAD_PER_DAY:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
 			case NajaPackage.PERSON__AVAILABLE_SCHEDULES:
