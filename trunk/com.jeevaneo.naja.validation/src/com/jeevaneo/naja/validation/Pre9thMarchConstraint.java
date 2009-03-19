cfc07c97cdf70fefe79784abb46e779443515fcf
alexandre.borgoltz <alexandre.borgoltz@9d19b4e0-ff7f-11dd-984d-ab748d24aa7e>
2009-03-19 07:28:25 +0000

    nightly work

---
+++ trunk/com.jeevaneo.naja.validation/src/com/jeevaneo/naja/validation/Pre9thMarchConstraint.java
@@ -0,0 +1,44 @@
+package com.jeevaneo.naja.validation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+
+import com.jeevaneo.naja.Imputation;
+import com.jeevaneo.naja.Person;
+
+public class Pre9thMarchConstraint extends AbstractModelConstraint {
+
+	private static Date march8;
+	static 
+	{
+		try {
+			march8 = new SimpleDateFormat("yyyyMMdd").parse("20090308");
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Pre9thMarchConstraint() {
+	}
+
+	@Override
+	public IStatus validate(IValidationContext ctx) {
+		if(!(ctx.getTarget() instanceof Imputation))
+		{
+			System.out.println("IGNORED: " + ctx.getTarget());
+			return ctx.createSuccessStatus();
+		}
+		Imputation imputation = (Imputation) ctx.getTarget();
+		if(null!=imputation.getDate() && imputation.getDate().after(march8))
+		{
+			return ctx.createFailureStatus(imputation.getDate(), imputation.getResource().getName(), imputation.getLoad(), imputation.getComment());
+		}
+		return ctx.createSuccessStatus();
+	}
+
+}
