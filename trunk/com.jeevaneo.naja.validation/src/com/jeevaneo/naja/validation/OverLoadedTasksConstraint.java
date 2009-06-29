7d3740b318de7bf672722b53c5e72bc8080ccf36
alexandre.borgoltz <alexandre.borgoltz@9d19b4e0-ff7f-11dd-984d-ab748d24aa7e>
2009-06-29 08:27:16 +0000

    aborgoltz: no duplicate warning (imputed/loaded)

--- trunk/com.jeevaneo.naja.validation/src/com/jeevaneo/naja/validation/OverLoadedTasksConstraint.java
+++ trunk/com.jeevaneo.naja.validation/src/com/jeevaneo/naja/validation/OverLoadedTasksConstraint.java
@@ -15,11 +15,17 @@ public class OverLoadedTasksConstraint extends AbstractModelConstraint {
 	public IStatus validate(IValidationContext ctx) {
 		System.out.print("OverLoadedTasksConstraint.validate() ");
 		if (!(ctx.getTarget() instanceof Task)) {
+
 			System.out.println();
 			return ctx.createSuccessStatus();
 		}
 		Task task = (Task) ctx.getTarget();
-		System.out.println(task);		
+		System.out.println(task);				
+		if(task.getImputedLoad()>task.getTotalLoad())
+		{
+			//that case is already checked by the overimputed constraint
+			return ctx.createSuccessStatus();
+		}
 			if (task.getImputedLoad()+task.getUnimputedPlanifiedLoad() > task.getTotalLoad()) {
 				return ctx.createFailureStatus(task.getLabel(), task
 						.getImputedLoad(),task
